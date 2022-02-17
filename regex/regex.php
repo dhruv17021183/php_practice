b6825fb3714a67c3d0bc82261f0c42e88aebe3fe
dhruv17021183 <84187932+dhruv17021183@users.noreply.github.com>
2022-02-17 19:42:07 +0530

    Create regex.php

---
+++ regex/regex.php
@@ -0,0 +1,60 @@
+<?php
+// $arr=array(
+//     array("dhruv","shyam","mayur"),
+//     array("priyank","jitesh","harsh"),
+//     array("puniut","mahesh","jatin")
+//            );
+// echo $arr[0][2]; 
+// $my_url="www.youtube.com";
+// if(preg_match("/youtube/",$my_url)){
+//     echo "The $my_url contains youtube";
+// }
+// else{
+//     echo "no";
+// }
+// $text="i am unable to work";
+// $my_array=preg_split("/ /",$text);
+// print_r($my_array);
+// lets took example of email checking
+// $email="dsuthar11@gmail.com";
+// if(preg_match("/^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\.[a-zA-Z.]{2,5}$/",$email)){
+//     echo "$email is valid email address";
+// }
+// else{
+//     echo "$email is not valid";
+// }
+// $string = "PHP is the web scripting  language of choice";
+// $exp=preg_match_all("/php|web|language|ap/i",$string,$array);
+// if($exp){
+//     echo "found!";
+// }
+// else{
+//     echo "Not found";
+// }
+//     echo "<pre>";
+//     print_r($array);
+//     echo "</pre>";
+//     echo $array[0][0];
+/* Modifiers 
+"i" perform a case-insensitive search
+"m" performs a multiline search
+"u" enables correct matching og utf-8 encoded patterns
+*/
+/* patterns 
+"[abc]" find one character from the option between the brackets
+"[^abc]" find any character not between the brackets
+"[0-9]" find one character from the range
+*/
+// $str="i lov pro python programming 533 lang-uage but not from my heart";
+// $exp=preg_match("/pro+gramming/",$str,$array); 
+// echo $exp;
+// if($exp){
+//     echo "yes";
+// }
+// else{
+//     echo "no";
+// }
+//     echo "<pre>";
+//     print_r($array);
+//     echo "</pre>";
+?>
